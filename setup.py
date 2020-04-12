81781e981f10f45a45b687de183bbc8aa5a30b40
Drew Volz <volz.drew@gmail.com>
2020-04-11 17:42:01 -0700

    Reformat the setup script

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 import setuptools
 
 setuptools.setup(
-	name="memberboat",
-	version="0.0.0",
-	package_data={},
-	classifiers=[
-		"Development Status :: 1 - Planning",
-		"Environment :: Console",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python",
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.6",
-		"Programming Language :: Python :: 3.7",
-		"Programming Language :: Python :: 3.8",
-		"Topic :: Software Development",
-	],
-	entry_points={
-		'console_scripts': [
-			'memberboat = memberboat.__main__:main'
-		],
-	},
-	python_requires=">=3.6",
-	install_requires=["argparse==1.4.0", "attrs==19.3.0", "requests==2.14.0", "PyGithub==1.47"],
+    name="memberboat",
+    version="0.0.0",
+    package_data={},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+    ],
+    entry_points={
+        'console_scripts': ['memberboat = memberboat.__main__:main'],
+    },
+    python_requires=">=3.6",
+    install_requires=[
+        "argparse==1.4.0", "attrs==19.3.0", "requests==2.14.0",
+        "PyGithub==1.47"
+    ],
 )
