af20652ddafb5aab3209187c89874dbebf8886fe
Drew Volz <volz.drew@gmail.com>
2020-04-11 18:38:39 -0700

    Format with yapf (#3)
    * Add yapf to the requirements
    * Reformat source files
    * Add workflow for checking yapf formatting
    * Add a yapf style config file
    
    Co-authored-by: Kristofer Rye <kristofer.rye@gmail.com>

--- memberboat/__main__.py
+++ memberboat/__main__.py
@@ -12,6 +12,7 @@ from github import Github, GithubIntegration
 #   memberboat apply -- apply the configuration
 #   - --dry-run -- only check and print what would happen
 
+
 def get_installation_token(owner, repo):
 	integration_id = os.environ['GITHUB_APP_ID']
 	private_key_file = os.environ['GITHUB_APP_PRIVATE_KEY_FILE']
@@ -19,22 +20,24 @@ def get_installation_token(owner, repo):
 	with open(private_key_file, "r") as file:
 		private_key = file.read().strip().encode()
 
-		integration = GithubIntegration(integration_id=int(integration_id), private_key=private_key)
+		integration = GithubIntegration(integration_id=int(integration_id),
+		                                private_key=private_key)
 
 		installation = integration.get_installation(owner, repo)
 
 		return integration.get_access_token(installation.id.value).token
 
 
-
 def main():
-	parser = argparse.ArgumentParser(prog='memberboat', description='Synchronize membership with GitHub')
+	parser = argparse.ArgumentParser(
+	    prog='memberboat', description='Synchronize membership with GitHub')
 
 	subparsers = parser.add_subparsers(help='sub-command help')
 
 	args = parser.parse_args()
 
-	token = get_installation_token(os.environ['GITHUB_ORG_NAME'], os.environ['GITHUB_REPO_NAME'])
+	token = get_installation_token(os.environ['GITHUB_ORG_NAME'],
+	                               os.environ['GITHUB_REPO_NAME'])
 
 	g = Github(token)
 
